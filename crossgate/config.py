3ce5614f9b11d33eb9fca37fc7ea0c705e54acc0
suyu0925 <suyu0925@gmail.com>
2018-07-08 05:55:08 +0800

    还是有点BUG，极低概率会在接任务时错误的识别

--- crossgate/config.py
+++ crossgate/config.py
@@ -1,5 +1,9 @@
 # -*- coding:utf-8 -*-
 
+MAX_FRAME = -1
+
+STOP_WHEN_BACKGROUND = False
+
 # 窗体标题  用于定位游戏窗体
 WINDOW_TITLE = "腾讯手游助手【极速傲引擎】"
 # 游戏区域距离顶点的长度
@@ -10,3 +14,14 @@ MARGIN_HEIGHT = 36
 GAME_WIDTH = 1024
 # 游戏区域高度
 GAME_HEIGHT = 576
+
+TIME_INTERVAL = 1
+
+# 战斗图标
+ACTION_RECT = (947, 484, 947 + 45, 484 + 45)
+
+# 第二个任务
+SECOND_TASK_RECT = (841, 233, 841 + 260, 233 + 60)
+
+# 接受委托
+ACCEPT_TASK_RECT = (818, 377, 818 + 165, 377 + 36)
--- crossgate/main.py
+++ crossgate/main.py
@@ -1,9 +1,17 @@
+import datetime
+import win32api
+import win32con
 import win32gui
 import time
 import cv2
 import numpy as np
 from PIL import ImageGrab
 from config import *
+
+assets = {
+    "accept_task": cv2.imread("./assets/accept_task.png"),
+    "battle_action": cv2.imread("./assets/battle_action.png")
+}
 
 # 获取窗体坐标位置(左上)
 
@@ -25,15 +33,95 @@ def getGameWindowRect():
 # 获取一张游戏截图
 
 
-def getGameImage(bbox):
+def getGameImage(bbox, save=False):
+  st = time.clock()
   print('捕获屏幕截图...')
   scim = ImageGrab.grab(bbox)  # 屏幕截图，获取到的是Image类型对象
   img = np.array(scim.getdata(), dtype='uint8').reshape(
-      GAME_WIDTH, GAME_HEIGHT, 3)
-  # scim.save('game.png')
+      GAME_HEIGHT, GAME_WIDTH, 3)
+  if save:
+    scim.save('game.png')
   # cv2img = cv2.imread("game.png")  # opencv 读取，拿到的是ndarray存储的图像
   # cv2.imwrite("game_cv2.png", cv2img)
+  print('捕获屏幕截图完成，耗时', time.clock() - st, '毫秒')
   return img
+
+
+def cropSecondTask(game_image, save=False):
+  img = game_image[SECOND_TASK_RECT[1]:SECOND_TASK_RECT[3],
+                   SECOND_TASK_RECT[0]:SECOND_TASK_RECT[2]]
+  if save:
+    cv2.imwrite("second_task.png", img)
+  return img
+
+
+def cropBattleAction(game_image, save=False):
+  img = game_image[ACTION_RECT[1]:ACTION_RECT[3],
+                   ACTION_RECT[0]:ACTION_RECT[2]]
+  if save:
+    cv2.imwrite("battle_action.png", img)
+  return img
+
+
+def cropAcceptTask(game_image, save=False):
+  img = game_image[ACCEPT_TASK_RECT[1]:ACCEPT_TASK_RECT[3],
+                   ACCEPT_TASK_RECT[0]:ACCEPT_TASK_RECT[2]]
+  if save:
+    cv2.imwrite("accept_task.png", img)
+  return img
+
+
+def nearlyEqual(a, b):
+  if a.size != b.size:
+    return False
+
+  size = a.size
+  same = np.sum(a == b)
+  print("same: ", same, "different: ", (size - same) / size)
+  return (size - same) / size < 0.01
+
+
+def isForeground():
+  text = win32gui.GetWindowText(win32gui.GetForegroundWindow())
+  return text == WINDOW_TITLE
+
+
+def click(x, y):
+  if x == None or y == None:
+    return
+
+  if x <= 0 or y <= 0:
+    return
+
+  foreground = isForeground()
+  if not foreground:
+    print('游戏窗口不在前台')
+
+  if foreground or STOP_WHEN_BACKGROUND == False:
+    game_rect = getGameWindowRect()
+    x = game_rect[0] + x
+    y = game_rect[1] + y
+
+    win32api.SetCursorPos((x, y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, x, y, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, x, y, 0, 0)
+    time.sleep(TIME_INTERVAL)
+
+
+def act(action):
+  if action == "accept_task":
+    print('接受委托')
+    x = ACCEPT_TASK_RECT[0]
+    y = ACCEPT_TASK_RECT[1]
+  elif action == "auto_battle":
+    print('开启自动战斗')
+    # TODO:
+  elif action == "second_task":
+    print('寻路第二个任务')
+    x = SECOND_TASK_RECT[0]
+    y = SECOND_TASK_RECT[1]
+
+  click(x, y)
 
 
 if __name__ == '__main__':
@@ -41,10 +129,33 @@ if __name__ == '__main__':
   game_rect = getGameWindowRect()
   time.sleep(1)
 
-  # 2、从屏幕截图一张，通过opencv读取
-  st = time.clock()
-  game_image = getGameImage(game_rect)
-  print('grab image cost', time.clock() - st, 'ms')
+  frame = 0
+  while MAX_FRAME != 0:
+    frame += 1
+
+    # 从屏幕截图一张，通过opencv读取
+    game_image = getGameImage(game_rect, save=True)
+
+    second_task = cropSecondTask(game_image, save=True)
+    battle_action = cropBattleAction(game_image, save=True)
+    accept_task = cropAcceptTask(game_image, save=True)
+
+    if nearlyEqual(accept_task, assets["accept_task"]):
+      print('提示框：接受委托')
+      act("accept_task")
+      act("second_task")
+      act("second_task")
+    elif nearlyEqual(battle_action, assets["battle_action"]):
+      print('自动战斗中')
+    else:
+      print('默认寻路第二个任务')
+      act("second_task")
+
+    time.sleep(3)
+    print('{}: 完成第{}次运行'.format(datetime.datetime.now(), frame))
+
+    if MAX_FRAME > 0 and frame >= MAX_FRAME:
+      break
 
   # 7、消除完成，释放资源。
   print('优雅的跑路')
